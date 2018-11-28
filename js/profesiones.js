42bcd7957e76f8414f931514da51a7c9e67a2484
chaveroricardo <chavero.ricardo@gmail.com>
2018-11-28 09:28:38 -0600

    empezando

---
+++ js/profesiones.js
@@ -0,0 +1,36 @@
+function Career(name){
+  this.name = name;
+  this.income = income;
+  this.dividend = dividend;
+  this.business = business;
+
+  this.profesion = profesion;
+
+  this.taxes = taxes;
+  this.mortgage = mortgage;
+  this.carLoan = carLoan;
+  this.creditCardLoan = creditCardLoan;
+  this.studentLoan = studentLoan;
+  this.otherExpenses = otherExpenses;
+  this.loanPayment = loanPayment;
+  this.child = child;
+
+  this.assetsGrowth = [];
+  this.assetsIncome = [];
+
+  this.liability = liability;
+  
+
+
+}
+
+
+
+
+/*
+[
+    "Piloto","Administrador","Médico",
+    "Abogado","Ingeniero","Conserje","FullStack Developer",
+    "Mecánico", "Conductor Uber", "Enfermero",
+    "Secretaria", "Profesor"
+  ]
