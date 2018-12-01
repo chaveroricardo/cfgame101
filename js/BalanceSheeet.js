90241de6a57340b982c663bacc5417369f7960e2
chaveroricardo <chavero.ricardo@gmail.com>
2018-12-01 16:27:50 -0600

    terminados los negocios

--- js/BalanceSheeet.js
+++ js/BalanceSheeet.js
@@ -55,6 +55,106 @@ var profesiones =
   ]
 
 
+var negociosStocks =[
+  {
+    name: "Acción - TSLA.MX",
+    description:       
+      ["La volatilidad de los mercados y el miedo por la guerra comercial ha llevado a un mínimo histórico", "Es una acción prometedora para un futuro sin hidrocarburos, TSLA trabaja muy fuerte para hacer de uso masivo sus automóviles", "Hay mucho optimismo en el mercado de valores, la acción está en sus máximos históricos"],
+    cost: [
+       function(){
+          var min=111;
+          var max=150;
+          var random = Math.floor(Math.random()*(+max- +min) + +min);
+          return random;
+        },
+       function(){
+          var min=650;
+          var max=950;
+          var random = Math.floor(Math.random()*(+max- +min) + +min);
+          return random;},
+       function(){
+            var min=1200;
+            var max=1500;
+            var random = Math.floor(Math.random()*(+max- +min) + +min);
+            return random;}
+    ],
+    tradingRange: "650 a 950",
+    numeroDeAcciones: 0,
+  },
+  {
+    name: "Acción - Fondo Indexado ETF's S&P500",
+    description: ["La volatilidad de los mercados y el miedo por la guerra comercial ha llevado a un mínimo histórico en el S&P 500","Invertir en fondos indexados es la forma más segura de hacer crecer tu dinero en el largo plazo","Se encuentra una economía sólida, las tasas de desempleo son bajas y las empresas generan valor. Estamos en máximos históricos"],
+    cost: [
+      function(){
+         var min=11;
+         var max=15;
+         var random = Math.floor(Math.random()*(+max- +min) + +min);
+         return random;
+       },
+      function(){
+         var min=110;
+         var max=340;
+         var random = Math.floor(Math.random()*(+max- +min) + +min);
+         return random;},
+      function(){
+           var min=300;
+           var max=500;
+           var random = Math.floor(Math.random()*(+max- +min) + +min);
+           return random;}
+          ],
+          tradingRange: "110 a 340",
+          numeroDeAcciones: 0,
+    },
+    {
+      name: " Acción - PG (Procter & Gamble)",
+      description: ["La volatilidad de los mercados y el miedo por la guerra comercial ha llevado a un mínimo histórico en el S&P 500","Invertir en fondos indexados es la forma más segura de hacer crecer tu dinero en el largo plazo","Se encuentra una economía sólida, las tasas de desempleo son bajas y las empresas generan valor. Estamos en máximos históricos"],
+      cost: [
+        function(){
+           var min=450;
+           var max=650;
+           var random = Math.floor(Math.random()*(+max- +min) + +min);
+           return random;
+         },
+        function(){
+           var min=750;
+           var max=1000;
+           var random = Math.floor(Math.random()*(+max- +min) + +min);
+           return random;},
+        function(){
+             var min=1100;
+             var max=1700;
+             var random = Math.floor(Math.random()*(+max- +min) + +min);
+             return random;}
+            ],
+            tradingRange: "750 a 100",
+            numeroDeAcciones: 0,
+      },
+      {
+        name: " Bitcoin",
+        description: ["La especulación de una nueva tecnología terminó con este mercado y la burbuja ha terminado","Es una tecnología innovadora y que puedes experimentar","El boom del bitcoin, todo mundo quiere entrar y está en sus máximos históricos"],
+        cost: [
+          function(){
+             var min=50;
+             var max=100;
+             var random = Math.floor(Math.random()*(+max- +min) + +min);
+             return random;
+           },
+          function(){
+             var min=4000;
+             var max=9000;
+             var random = Math.floor(Math.random()*(+max- +min) + +min);
+             return random;},
+          function(){
+               var min=20000;
+               var max=22000;
+               var random = Math.floor(Math.random()*(+max- +min) + +min);
+               return random;}
+              ],
+              tradingRange: "750 a 100",
+              numeroDeAcciones: 0,
+        }
+];
+
 var negociosIncomeSmall = [
     {
       name: "Empieza un blog",
@@ -85,51 +185,23 @@ var negociosIncomeSmall = [
       downPay: 2000,
     },
     {
-      name: "Acción - TSLA.MX",
-      description:       
-        ["La volatilidad de los mercados y el miedo por la guerra comercial ha llevado a un mínimo histórico", "Es una acción prometedora para un futuro sin hidrocarburos, TSLA trabaja muy fuerte para hacer de uso masivo sus automóviles", "Es una momento de máximos históricos, el optimismo por los mercados hace que llegue a su máximo histórico"],
-      cost: [
-         function(){
-            var min=111;
-            var max=150;
-            var random = Math.floor(Math.random()*(+max- +min) + +min);
-            return random;
-          },
-         function(){
-            var min=650;
-            var max=950;
-            var random = Math.floor(Math.random()*(+max- +min) + +min);
-            return random;},
-         function(){
-              var min=1200;
-              var max=1500;
-              var random = Math.floor(Math.random()*(+max- +min) + +min);
-              return random;}
-      ],
-      tradingRange: "450 a 950",
-      numeroDeAcciones: 0,
-    },
-    {
       name: "Acción - Cetes",
       description: "Aprovecha ahora que hay tasas máximas en renta fija",
       cost:100,
       cashFlow: .66,
       numeroDeCetes: 0,
     },
+    
     {
-      name: "Fondo Indexado ETF's S&P500",
-      description: "Invertir en fondos indexados es la forma más segura de hacer crecer tu dinero en el largo plazo",
-      cost:function(){
-        var min=111;
-        var max=150;
-        var random = Math.floor(Math.random()*(+max- +min) + +min);
-        return random;
-      },
-      tradingRange: "450 a 650",
-      numeroDeAcciones: 0,
-    }
+      name: "Invierte en una startup como partime",
+      description: "Te asocias con alguien que tiene ideas para el futuro, medio tiempo para no dejar tu trabajo",
+      cost: 70000,
+      cashFlow: 0,
+      downPay: 30000,
+    },
+    
 
-  ];
+  ]
     
 var negociosIncomeBig = [
       {
@@ -146,4 +218,18 @@ var negociosIncomeBig = [
         cashFlow: 35816.67,
         downPay: 1750000,
       },
+      {
+        name: "Franquicia de Comida Rápida",
+        description: "Una de las franquicias más conocidas de comida rápida de la CDMX, garantizado un éxito",
+        cost: 4000000,
+        cashFlow: 112500,
+        downPay: 1000000,
+      },
+      {
+        name: "Peluquería",
+        description: "Negocio tradicional de corte de cabello, por necesidad los dueños lo venden",
+        cost: 300000,
+        cashFlow: 20000,
+        downPay: 300000,
+      }
     ]
--- js/financialops.js
+++ js/financialops.js
@@ -37,3 +37,41 @@ var random_num = rand(0, weighed_list.length-1);
 
 return weighed_list[random_num];
 }
+
+
+function probabilidad2(){
+  var rand = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var generateWeighedList = function(list, weight) {
+  var weighed_list = [];
+   
+  // Loop over weights
+  for (var i = 0; i < weight.length; i++) {
+      var multiples = weight[i] * 100;
+       
+      // Loop over the list of items
+      for (var j = 0; j < multiples; j++) {
+          weighed_list.push(list[i]);
+      }
+  }
+   
+  return weighed_list;
+};
+var  low = 0;
+
+
+var middle = 1;
+
+var high = 2;
+
+
+var list = [low,middle,high];
+var weight = [0.8, 0.175, 0.025];
+var weighed_list = generateWeighedList(list, weight);
+
+var random_num = rand(0, weighed_list.length-1);
+
+return weighed_list[random_num];
+}
