089a832695d9bbee1aa292e37612728427ded4e0
chaveroricardo <chavero.ricardo@gmail.com>
2018-12-11 20:10:52 -0600

    cambios

--- js/main.js
+++ js/main.js
@@ -14,11 +14,12 @@ var ctx;
 document.getElementById("spin").addEventListener("click", spin);
 
 function byte2Hex(n) {
-
+  var nybHexString = "0123456789ABCDEF";
+  return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
 }
-
+//color
 function RGB2Color(r,g,b) {
-
+  return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
 }
 
 function getColor(item, maxitem) {
